7ec4c8c012d5bea0c91441c9dd2734531b3818ef
DrCBeatz <brynecarruthers@gmail.com>
2023-10-28 23:20:50 -0400

    add supplier info modal and transitions (WIP)

--- frontend/vendor-search/src/App.tsx
+++ frontend/vendor-search/src/App.tsx
@@ -3,6 +3,9 @@
 import React from "react";
 import "./App.css";
 import VendorSearch from "./components/VendorSearch/VendorSearch";
+import Modal from "react-modal";
+
+Modal.setAppElement('#root');
 
 const App: React.FC = () => {
   return (
--- frontend/vendor-search/src/components/VendorsTable/VendorsTable.tsx
+++ frontend/vendor-search/src/components/VendorsTable/VendorsTable.tsx
@@ -1,6 +1,8 @@
 // VendorsTable.tsx
-
+import React, { useState, useEffect } from "react";
+import Modal from "react-modal";
 import "./VendorsTable.css";
+import "./VendorsTableModal.css";
 
 export interface Vendor {
   id: number;
@@ -19,45 +21,144 @@ interface VendorsTableProps {
   vendors: Vendor[];
 }
 
-const VendorsTable: React.FC<VendorsTableProps> = ({ vendors }) => (
-  <table className="vendors-table">
-    <thead>
-      <tr>
-        <th className="vendors-table__header-cell">Vendor</th>
-        <th className="vendors-table__header-cell">Suppliers</th>
-        <th className="vendors-table__header-cell">Contact Name</th>
-        <th className="vendors-table__header-cell">Contact Email</th>
-        <th className="vendors-table__header-cell">Website</th>
-        <th className="vendors-table__header-cell">Phone</th>
-        <th className="vendors-table__header-cell">Categories</th>
-      </tr>
-    </thead>
-    <tbody>
-      {vendors.map((vendor) => (
-        <tr className="vendors-table__row" key={vendor.id}>
-          <td className="vendors-table__cell">{vendor.name}</td>
-          <td className="vendors-table__cell">
-            {vendor.suppliers.map((supplier) => supplier.name).join(", ")}
-          </td>
-          <td className="vendors-table__cell">
-            {vendor.suppliers[0]?.contact_name || "-"}
-          </td>
-          <td className="vendors-table__cell">
-            {vendor.suppliers[0]?.contact_email || "-"}
-          </td>
-          <td className="vendors-table__cell">
-            {vendor.suppliers[0]?.website || "-"}
-          </td>
-          <td className="vendors-table__cell">
-            {vendor.suppliers[0]?.phone || "-"}
-          </td>
-          <td className="vendors-table__cell">
-            {vendor.categories.map((category) => category.name).join(", ")}
-          </td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
-);
+const VendorsTable: React.FC<VendorsTableProps> = ({ vendors }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentSupplier, setCurrentSupplier] = useState<
+    null | Vendor["suppliers"][0]
+  >(null);
+  const [closeAnimationStarted, setCloseAnimationStarted] = useState(false);
+
+  const openModal = (supplier: Vendor["suppliers"][0]) => {
+    setCurrentSupplier(supplier);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setCloseAnimationStarted(true);
+  };
+
+  useEffect(() => {
+    const modalContent = document.querySelector(
+      ".vendors-table__modal-content"
+    );
+    if (modalContent) {
+      modalContent.addEventListener("transitionend", () => {
+        if (closeAnimationStarted) {
+          setIsModalOpen(false);
+          setCloseAnimationStarted(false);
+        }
+      });
+    }
+  }, [closeAnimationStarted]);
+
+  return (
+    <div>
+      <table className="vendors-table">
+        <thead>
+          <tr>
+            <th className="vendors-table__header-cell">Vendor</th>
+            <th className="vendors-table__header-cell">Suppliers</th>
+            <th className="vendors-table__header-cell">Categories</th>
+          </tr>
+        </thead>
+        <tbody>
+          {vendors.map((vendor) => (
+            <tr className="vendors-table__row" key={vendor.id}>
+              <td className="vendors-table__cell">{vendor.name}</td>
+              <td className="vendors-table__cell">
+                {vendor.suppliers.flatMap((supplier, index, array) => {
+                  const elements = [];
+                  if (
+                    supplier.contact_name ||
+                    supplier.contact_email ||
+                    supplier.website ||
+                    supplier.phone
+                  ) {
+                    elements.push(
+                      <span
+                        key={supplier.name}
+                        onClick={() => openModal(supplier)}
+                        style={{
+                          cursor: "pointer",
+                          color: "blue",
+                          textDecoration: "underline",
+                        }}
+                      >
+                        {supplier.name}
+                      </span>
+                    );
+                  } else {
+                    elements.push(supplier.name);
+                  }
+
+                  // If it's not the last supplier, append a comma and a space
+                  if (index !== array.length - 1) {
+                    elements.push(", ");
+                  }
+                  return elements;
+                })}
+              </td>
+
+              <td className="vendors-table__cell">
+                {vendor.categories.map((category) => category.name).join(", ")}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        contentLabel="Supplier Details"
+        className={{
+          base: "vendors-table__modal-content",
+          afterOpen: "vendors-table__modal-content--after-open",
+          beforeClose: closeAnimationStarted
+            ? "vendors-table__modal-content--before-close"
+            : "",
+        }}
+        overlayClassName={{
+          base: "vendors-table__modal-overlay",
+          afterOpen: "vendors-table__modal-overlay--after-open",
+          beforeClose: closeAnimationStarted
+            ? "vendors-table__modal-overlay--before-close"
+            : "",
+        }}
+      >
+        <h2 className="vendors-table__modal-header">{currentSupplier?.name}</h2>
+        <p>
+          <strong>Contact Name:</strong> {currentSupplier?.contact_name}
+        </p>
+        <p>
+          <strong>Contact Email:</strong> {currentSupplier?.contact_email}
+        </p>
+        <p>
+          <strong>Website: </strong>
+          {currentSupplier?.website ? (
+            <a
+              href={currentSupplier.website}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {currentSupplier.website}
+            </a>
+          ) : (
+            "-"
+          )}
+        </p>
+        <p>
+          <strong>Phone:</strong> {currentSupplier?.phone}
+        </p>
+        <button
+          className="vendors-table__modal-close-button"
+          onClick={closeModal}
+        >
+          Close
+        </button>
+      </Modal>
+    </div>
+  );
+};
 
 export default VendorsTable;
