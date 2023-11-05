90388ff819b89de81de7d33f1dad40ab9f491672
DrCBeatz <brynecarruthers@gmail.com>
2023-11-05 18:08:25 -0500

    add tests for password encryption/decryption and modified admin form

--- cmsa/tests/test_models.py
+++ cmsa/tests/test_models.py
@@ -49,3 +49,65 @@ def test_vendor_relationships():
     assert supplier2 in vendor.suppliers.all()
     assert category1 in vendor.categories.all()
     assert category2 in vendor.categories.all()
+
+
+@pytest.mark.django_db
+def test_password_encryption_and_decryption():
+    # Sample data
+    plaintext_password = "sample_password"
+
+    # Create a supplier instance with the plaintext password
+    supplier = Supplier(name="Test Supplier", website_password=plaintext_password)
+    supplier.save()
+
+    # Check if the saved passwod is encrypted (i.e., different from plaintext)
+    assert supplier.website_password != plaintext_password
+
+    # Check decryption
+    decrypted_password = supplier.decrypt_password()
+    assert decrypted_password == plaintext_password
+
+    # Clean up
+    supplier.delete()
+
+@pytest.mark.django_db
+def test_password_encryption_on_edit():
+    plaintext_password = "initial_password"
+    updated_password = "updated_password"
+    
+    supplier = Supplier(name="Test Supplier", website_password=plaintext_password)
+    supplier.save()
+
+    supplier.website_password = updated_password
+    supplier.save()
+
+    assert supplier.website_password != updated_password
+
+    decrypted_password = supplier.decrypt_password()
+    assert decrypted_password == updated_password
+
+
+@pytest.mark.django_db
+def test_password_stays_encrypted_on_retrieve():
+    plaintext_password = "sample_password"
+    
+    supplier = Supplier(name="Test Supplier", website_password=plaintext_password)
+    supplier.save()
+
+    retrieved_supplier = Supplier.objects.get(pk=supplier.pk)
+    assert retrieved_supplier.website_password != plaintext_password
+
+    decrypted_password = retrieved_supplier.decrypt_password()
+    assert decrypted_password == plaintext_password
+
+
+@pytest.mark.django_db
+def test_admin_form_display_password(admin_client):
+    plaintext_password = "sample_password"
+    
+    supplier = Supplier(name="Test Supplier", website_password=plaintext_password)
+    supplier.save()
+
+    response = admin_client.get(f'/admin/cmsa/supplier/{supplier.pk}/change/')
+    assert 'original_website_password' in response.context
+    assert response.context['original_website_password'] == plaintext_password
