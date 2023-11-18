9e887971f12494b64796485968bb6c3ae3cffe6a
DrCBeatz <brynecarruthers@gmail.com>
2023-11-17 23:24:11 -0500

    add test_token_refresh & test_invalid_token_refresh to test_auth_endpoints.py

--- core/test_auth_endpoints.py
+++ core/test_auth_endpoints.py
@@ -2,6 +2,7 @@
 
 import pytest
 from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 @pytest.fixture
@@ -23,3 +24,33 @@ def test_token_obtain_pair(client, create_test_user):
     assert response.status_code == 200
     assert "access" in response.json()
     assert "refresh" in response.json()
+
+
+@pytest.mark.django_db
+def test_token_refresh(client, create_test_user):
+    # create_test_user is a fixture; it returns a user object
+    user = create_test_user
+
+    # Obtain a refresh token
+    refresh = RefreshToken.for_user(user)
+
+    # Test the token refresh endpoint
+    url = "/api/token/refresh/"
+    data = {
+        "refresh": str(refresh),
+    }
+    response = client.post(url, data)
+
+    assert response.status_code == 200
+    assert "access" in response.json()
+
+
+@pytest.mark.django_db
+def test_invalid_token_refresh(client, create_test_user):
+    url = "/api/token/refresh/"
+    data = {
+        "refresh": "invalid_token",
+    }
+    response = client.post(url, data)
+
+    assert response.status_code == 401
