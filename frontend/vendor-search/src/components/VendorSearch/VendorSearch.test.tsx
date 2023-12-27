34930fa260787b2a20e3d2c988fa662393ecb0a4
DrCBeatz <brynecarruthers@gmail.com>
2023-12-26 21:43:45 -0500

    fix 'renders components correctly on initial load' & 'populates VendorsTable on successful data fetch'

--- frontend/vendor-search/src/components/VendorSearch/VendorSearch.test.tsx
+++ frontend/vendor-search/src/components/VendorSearch/VendorSearch.test.tsx
@@ -1,135 +1,116 @@
-// // VendorSearch.test.tsx
+// VendorSearch.test.tsx
 
-// import {
-//   render,
-//   screen,
-//   fireEvent,
-//   within,
-//   waitFor,
-// } from "@testing-library/react";
-// import VendorSearch from "./VendorSearch";
-// import { vi } from "vitest";
-// import { Vendor } from "../VendorsTable/VendorsTable";
-
-// type FetchResponse = {
-//   ok: boolean;
-//   status?: number;
-//   json: () => Promise<unknown>;
-// };
-
-// type FetchMock = typeof fetch & {
-//   mockResolvedValueOnce: (response: FetchResponse) => void;
-//   mockRejectedValueOnce: (error: Error) => void;
-// };
+import {
+  render,
+  screen,
+  fireEvent,
+  within,
+  // waitFor,
+} from "@testing-library/react";
+import VendorSearch from "./VendorSearch";
+import { vi } from "vitest";
 
 describe("VendorSearch", () => {
-  test("dummy test", () => {
-    expect(true).toBe(true);
+  
+  beforeEach(() => {
+    vi.mock("import.meta", () => ({
+      env: { VITE_API_BASE_URL: "http://localhost:8000" },
+    }));
+    vi.clearAllMocks();
+
+    global.fetch = vi.fn((url: RequestInfo | URL, options?: RequestInit) => {
+      console.log(`Fetching ${url} with options:`, options);
+      const response = new Response(JSON.stringify([]), {
+        status: 200,
+        statusText: "OK",
+        headers: { "Content-Type": "application/json" },
+      });
+
+      return Promise.resolve(response);
+    }) as typeof fetch;
   });
 
-  // const createFetchResponse = (data: Vendor[]): FetchResponse => {
-  //   return {
-  //     ok: true,
-  //     json: () => new Promise((resolve) => resolve(data)),
-  //   };
-  // };
-  // beforeEach(() => {
-  //   vi.mock("import.meta", () => ({
-  //     env: { VITE_API_BASE_URL: "http://localhost:8000" },
-  //   }));
-  //   vi.clearAllMocks();
 
-  //   global.fetch = vi.fn((url: RequestInfo | URL, options?: RequestInit) => {
-  //     console.log(`Fetching ${url} with options:`, options);
-  //     const response = new Response(JSON.stringify([]), {
-  //       status: 200,
-  //       statusText: "OK",
-  //       headers: { "Content-Type": "application/json" },
-  //     });
+  test("renders components correctly on initial load", () => {
+    // Mock the onSearch function
+    const mockOnSearch = vi.fn();
+  
+    // Render the component with empty vendors and the mock function
+    render(<VendorSearch isUserLoggedIn={false} vendors={[]} onSearch={mockOnSearch} lastSearchTerm="" />);
+  
+    // Expectations for initial render:
+    expect(screen.getByRole("searchbox")).toBeInTheDocument();
+    expect(screen.getByRole("table")).toBeInTheDocument();
+  
+    // Ensure the table is initially empty:
+    const table = screen.getByRole("table");
+    const rowgroups = within(table).getAllByRole("rowgroup");
+    const tbody = rowgroups[1]; // This will select <tbody>
+  
+    expect(tbody).not.toHaveTextContent(/Vendor/);
+  });
+  
+  test("populates VendorsTable on successful data fetch", async () => {
+    const mockData = [
+      {
+        id: 1,
+        name: "Vendor 1",
+        suppliers: [{ name: "Supplier A" }],
+        categories: [{ name: "Category X" }],
+      },
+    ];
+  
+    // Mock the onSearch function to resolve with mockData
+    const mockOnSearch = vi.fn().mockResolvedValue(mockData);
+  
+    render(<VendorSearch isUserLoggedIn={false} vendors={mockData} onSearch={mockOnSearch} lastSearchTerm="" />);
+  
+    // Simulate a search action
+    const searchButton = screen.getByRole("button", { name: /search/i });
+    fireEvent.click(searchButton);
+  
+    // Expect the VendorsTable to be populated with the mock data
+    const displayedVendor = await screen.findByText("Vendor 1");
+  
+    expect(displayedVendor).toBeInTheDocument();
+  });
 
-  //     return Promise.resolve(response);
-  //   }) as typeof fetch;
-  // });
-
-
-  // test("renders components correctly on initial load", () => {
-  //   render(<VendorSearch isUserLoggedIn={false} />);
-
-  //   // Expectations for initial render:
-  //   expect(screen.getByRole("searchbox")).toBeInTheDocument(); // This assumes SearchForm renders an input of type "search"
-  //   expect(screen.getByRole("table")).toBeInTheDocument(); // This assumes VendorsTable renders a <table> element
-
-  //   // Ensure the table is initially empty:
-  //   const table = screen.getByRole("table");
-  //   const rowgroups = within(table).getAllByRole("rowgroup");
-  //   const tbody = rowgroups[1]; // This will select <tbody>
-
-  //   expect(tbody).not.toHaveTextContent(/Vendor/);
-
-  //   expect(within(tbody).queryByText(/Supplier/)).not.toBeInTheDocument();
-
-  //   expect(screen.queryByText(/Category/)).not.toBeInTheDocument();
-  // });
-
-  // test("populates VendorsTable on successful data fetch", async () => {
-  //   const mockData = [
-  //     {
-  //       id: 1,
-  //       name: "Vendor 1",
-  //       suppliers: [{ name: "Supplier A" }],
-  //       categories: [{ name: "Category X" }],
-  //     },
-  //   ];
-
-  //   (fetch as FetchMock).mockResolvedValueOnce(createFetchResponse(mockData));
-
-  //   render(<VendorSearch isUserLoggedIn={false} />);
-
-  //   console.log("VendorSearch component rendered.");
-
-  //   // Simulate a search action
-  //   const searchButton = screen.getByRole("button", { name: /search/i });
-  //   fireEvent.click(searchButton);
-
-  //   console.log("Search button clicked.");
-
-  //   // Expect the VendorsTable to be populated with the mock data
-  //   const displayedVendor = await screen.findByText("Vendor 1");
-
-  //   expect(displayedVendor).toBeInTheDocument();
-  // });
-
-  // /**
-  //  * This test verifies that the VendorSearch component handles network errors gracefully.
-  //  * When the fetch request to retrieve vendors fails, the error should be logged to the console.
-  //  *
-  //  * Note: The 'Network error' that appears in test logs is expected and is intentionally triggered by this test.
-  //  */
   // test("handles errors on failed data fetch", async () => {
-  //   // Intentionally mock a failed fetch request to simulate a network error
-  //   const mockError = "Network error";
-  //   (fetch as FetchMock).mockRejectedValueOnce(new Error(mockError));
-
+  //   // Mock the fetch function to simulate a network error
+  //   global.fetch = vi.fn().mockRejectedValue(new Error("Network error"));
+  
   //   // Spy on console.error to verify if the error gets logged as expected
   //   const consoleSpy = vi.spyOn(console, "error");
-
-  //   render(<VendorSearch isUserLoggedIn={false} />);
-
-  //   // Simulate a search action, which should trigger the fetch request
-  //   const searchButton = screen.getByRole("button", { name: /search/i });
-  //   fireEvent.click(searchButton);
-
+  
+  //   // Mock the searchVendors function to simulate its behavior on error
+  //   const mockSearchVendors = vi.fn().mockImplementation(() => {
+  //     // Simulate what searchVendors does, including the error handling
+  //     throw new Error("Network error");
+  //   });
+  
+  //   // Render the component with the mock searchVendors function
+  //   render(<VendorSearch isUserLoggedIn={true} vendors={[]} onSearch={mockSearchVendors} lastSearchTerm="test" />);
+  
+  //   // Simulate typing into the search input
+  //   const searchInput = screen.getByPlaceholderText("Search vendors");
+  //   fireEvent.change(searchInput, { target: { value: "test query" } });
+  
+  //   // Simulate submitting the form
+  //   const form = screen.getByRole('form');
+  //   fireEvent.submit(form);
+  
   //   // Use waitFor to ensure all asynchronous operations inside the component have completed
   //   await waitFor(() => {
   //     // Check if the expected error message is logged to the console
   //     expect(consoleSpy).toHaveBeenCalledWith(
   //       "Error fetching data: ",
-  //       expect.objectContaining({ message: mockError })
+  //       expect.objectContaining({ message: "Network error" })
   //     );
   //   });
-
-  //   // Cleanup the spy to restore the original console.error behavior
+  
+  //   // Cleanup the spy and mocks to restore the original behavior
   //   consoleSpy.mockRestore();
+  //   global.fetch.mockRestore();
   // });
 
   // test("calls searchVendors with the correct term on form submit", async () => {
