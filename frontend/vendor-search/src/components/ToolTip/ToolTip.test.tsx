e4144e5f6fa08d4ba697ce9d3de01ee525b1e424
DrCBeatz <brynecarruthers@gmail.com>
2023-12-29 21:44:30 -0500

    add more tests to ToolTip component

--- frontend/vendor-search/src/components/ToolTip/ToolTip.test.tsx
+++ frontend/vendor-search/src/components/ToolTip/ToolTip.test.tsx
@@ -29,4 +29,33 @@ describe("ToolTip Component", () => {
     await new Promise((r) => setTimeout(r, 0));
     expect(screen.queryByText("Tooltip content")).not.toBeVisible();
   });
+
+  test("tooltip does not cause unnecessary re-renders", () => {
+    const renderSpy = vi.fn();
+    const TestComponent = () => {
+      renderSpy();
+      return <ToolTip content="Tooltip content">Hover over me</ToolTip>;
+    };
+
+    render(<TestComponent />);
+    expect(renderSpy).toHaveBeenCalledTimes(1);
+  });
+
+  test("tooltip content has correct visibility classes on hover", async () => {
+    render(<ToolTip content="Tooltip content">Hover over me</ToolTip>);
+    const triggerElement = screen.getByText("Hover over me");
+    const tooltipContent = screen.getByText("Tooltip content");
+
+    // Initially, tooltip content should have the 'hidden' class
+    expect(tooltipContent).toHaveClass("tooltip__content--hidden");
+
+    // Simulate mouse entering the tooltip trigger
+    fireEvent.mouseEnter(triggerElement);
+    expect(tooltipContent).toHaveClass("tooltip__content--visible");
+
+    // Simulate mouse leaving the tooltip trigger
+    fireEvent.mouseLeave(triggerElement);
+    await new Promise((r) => setTimeout(r, 0)); // Wait for the next "tick"
+    expect(tooltipContent).toHaveClass("tooltip__content--hidden");
+  });
 });
