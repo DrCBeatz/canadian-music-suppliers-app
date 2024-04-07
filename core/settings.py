b7fe00fd034ad03d86c6febcf4f495dcdc3db75f
DrCBeatz <brynecarruthers@gmail.com>
2024-04-07 19:09:07 -0400

    add domain name to settings.py and frontend build

--- core/settings.py
+++ core/settings.py
@@ -16,6 +16,7 @@ DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
 ALLOWED_HOSTS = [
     "secure-falls-59693-7d816c7f067e.herokuapp.com",
+    "www.canadamusicsuppliers.ca",
     "localhost",
     "127.0.0.1",
     "testserver",
